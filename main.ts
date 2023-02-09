a955e2cac83ded8d3089886471adffa0a9fe171f
laraaabrrio <122534361+laraaabrrio@users.noreply.github.com>
2023-02-09 13:49:05 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,25 +1,4 @@
-input.onButtonPressed(Button.A, function () {
-    NUMERO += 1
-    basic.showNumber(NUMERO)
+radio.onReceivedString(function (receivedString) {
+    led.plot(parseFloat(receivedString.substr(0, 1)), parseFloat(receivedString.substr(1, 1)))
 })
-input.onButtonPressed(Button.B, function () {
-    NUMERO += -1
-    basic.showNumber(NUMERO)
-})
-let NUMERO = 0
-NUMERO = 0
-basic.showNumber(NUMERO)
-basic.forever(function () {
-    if (input.compassHeading() > 315 || input.compassHeading() < 45) {
-        basic.showArrow(ArrowNames.North)
-    }
-    if (input.compassHeading() > 45 && input.compassHeading() < 135) {
-        basic.showArrow(ArrowNames.West)
-    }
-    if (input.compassHeading() > 135 && input.compassHeading() < 225) {
-        basic.showArrow(ArrowNames.South)
-    }
-    if (input.compassHeading() > 225 && input.compassHeading() < 315) {
-        basic.showArrow(ArrowNames.East)
-    }
-})
+radio.setGroup(1)
