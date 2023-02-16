53443898c934692522a2f18fbd8200ce9e4ad406
laraaabrrio <122534361+laraaabrrio@users.noreply.github.com>
2023-02-16 13:51:16 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,22 @@
+input.onButtonPressed(Button.A, function () {
+    x += 1
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.setGroup(x * 10 / y)
+    radio.sendString("lara")
+    radio.setGroup(1)
+    y = 0
+    x = 0
+    basic.clearScreen()
+})
 radio.onReceivedString(function (receivedString) {
     led.plot(parseFloat(receivedString.substr(0, 1)), parseFloat(receivedString.substr(1, 1)))
 })
+input.onButtonPressed(Button.B, function () {
+    y += 1
+})
+let y = 0
+let x = 0
 radio.setGroup(1)
+x = 0
+y = 0
